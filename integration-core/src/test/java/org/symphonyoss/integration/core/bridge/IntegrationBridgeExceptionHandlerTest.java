8807e87e7fa2e5b8dd2ee826609b2dfd8d6eeebe
Milton Gonçalves Quilzini <mquilzini@symphony.com>
2016-12-20 15:27:59 -0200

    Now, when dealing with issues while trying to notify an user from the impossibility to post a message to a room, the integration bridge will try to determine the room name based on instance info it already has.

--- integration-core/src/test/java/org/symphonyoss/integration/core/bridge/IntegrationBridgeExceptionHandlerTest.java
+++ integration-core/src/test/java/org/symphonyoss/integration/core/bridge/IntegrationBridgeExceptionHandlerTest.java
@@ -53,11 +53,15 @@ public class IntegrationBridgeExceptionHandlerTest {
 
   private static final String INTEGRATION_USER = "jirawebhook";
 
+  private static final String INSTANCE_NAME = "Project 1";
+
   private static final String DISPLAY_NAME = "JIRA";
 
-  private static final String ROOM_NAME = "Test Room";
+  private static final String STREAM = "81NYrj5fWcB2BxlVZQmeRX___qjLh236dA";
 
-  private static final String STREAM = "stream1";
+  private static final String STREAM_ID = "81NYrj5fWcB2BxlVZQmeRX///qjLh236dA==";
+
+  private static final String STREAM_ID_ALT = "dsaDSAD1S56D/1Q0//WqjLdsA==";
 
   private static final String IM = "im";
 
@@ -108,10 +112,23 @@ public class IntegrationBridgeExceptionHandlerTest {
 
   private ConfigurationInstance mockInstance() throws JsonProcessingException {
     String optionalProperties =
-        "{ \"lastPostedDate\": 1, \"owner\": \"" + USER_ID + "\", \"streams\": [ \"" + STREAM
-            + "\"] }";
+        "{ \"lastPostedDate\": 1, \"owner\": \"" + USER_ID + "\", \"streams\": [ \"" + STREAM + "\"], "
+            + "\"streamType\" : \"CHATROOM\" , \"rooms\" : [ { \"streamId\" : \"" + STREAM_ID + "\" , "
+            + "\"roomName\" : \"Test Room\"}]}";
 
     ConfigurationInstance instance = new ConfigurationInstance();
+    instance.setOptionalProperties(optionalProperties);
+    return instance;
+  }
+
+  private ConfigurationInstance mockInstanceAlt() throws JsonProcessingException {
+    String optionalProperties =
+        "{ \"lastPostedDate\": 1, \"owner\": \"" + USER_ID + "\", \"streams\": [ \"" + STREAM + "\"], "
+            + "\"streamType\" : \"CHATROOM\" , \"rooms\" : [ { \"streamId\" : \"" + STREAM_ID_ALT + "\" , "
+            + "\"roomName\" : \"Test Room\"}]}";
+
+    ConfigurationInstance instance = new ConfigurationInstance();
+    instance.setName(INSTANCE_NAME);
     instance.setOptionalProperties(optionalProperties);
     return instance;
   }
@@ -168,12 +185,6 @@ public class IntegrationBridgeExceptionHandlerTest {
     userInfo.setDisplayName(DISPLAY_NAME);
     when(usersApi.v2UserGet(TOKEN, null, null, INTEGRATION_USER, true)).thenReturn(userInfo);
 
-    V2RoomDetail roomDetail = new V2RoomDetail();
-    V2RoomAttributes roomAttributes = new V2RoomAttributes();
-    roomAttributes.setName(ROOM_NAME);
-    roomDetail.setRoomAttributes(roomAttributes);
-    doReturn(roomDetail).when(streamService).getRoomInfo(INTEGRATION_USER, STREAM);
-
     doAnswer(new Answer<V2Message>() {
       @Override
       public V2Message answer(InvocationOnMock invocationOnMock) throws Throwable {
@@ -196,6 +207,50 @@ public class IntegrationBridgeExceptionHandlerTest {
     assertEquals(
         "<messageML>JIRA has been removed from Test Room, I can no longer post messages in Test "
             + "Room unless I am reconfigured to do so.</messageML>",
+        messagePosted);
+  }
+
+  @Test
+  public void testForbiddenPostMessageSuccessfullyForUndeterminedRoom()
+      throws com.symphony.api.agent.client.ApiException, IntegrationConfigException, IOException,
+      com.symphony.api.pod.client.ApiException {
+    ConfigurationInstance instance = mockInstanceAlt();
+
+    mockAuthForbidden();
+    mockConfigurationService();
+
+    when(authenticationProxy.getSessionToken(INTEGRATION_USER)).thenReturn(TOKEN);
+
+    Stream resultIM = new Stream();
+    resultIM.setId(IM);
+    doReturn(resultIM).when(streamService).createIM(INTEGRATION_USER, new Long(USER_ID));
+
+    UserV2 userInfo = new UserV2();
+    userInfo.setDisplayName(DISPLAY_NAME);
+    when(usersApi.v2UserGet(TOKEN, null, null, INTEGRATION_USER, true)).thenReturn(userInfo);
+
+    doAnswer(new Answer<V2Message>() {
+      @Override
+      public V2Message answer(InvocationOnMock invocationOnMock) throws Throwable {
+        V2MessageSubmission messageSubmission =
+            (V2MessageSubmission) invocationOnMock.getArguments()[2];
+        messagePosted = messageSubmission.getMessage();
+        V2Message message = new V2Message();
+        message.setMessage(messageSubmission.getMessage());
+        return message;
+      }
+    }).when(streamService).postMessage(eq(INTEGRATION_USER), eq(IM), any(V2MessageSubmission
+        .class));
+
+    exceptionHandler.handleRemoteApiException(new RemoteApiException(403, new ApiException()),
+        instance, INTEGRATION_USER, "", STREAM);
+
+    List<String> streams =
+        WebHookConfigurationUtils.getStreams(savedInstance.getOptionalProperties());
+    assertTrue(streams.isEmpty());
+    assertEquals(
+        "<messageML>JIRA has been removed from a room belonging to web hook instance Project 1, I can no longer post "
+            + "messages for some of the rooms in this instance unless I am reconfigured to do so.</messageML>",
         messagePosted);
   }
 
