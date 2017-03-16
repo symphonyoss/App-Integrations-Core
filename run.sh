e19c5de0b33c4873f6bafbe65b249115d01259a7
Maurizio Pillitu <maoo@symphony.foundation>
2017-03-16 22:39:09 +0100

    added application.yaml.template

--- run.sh
+++ run.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-# Run integrations locally by simulating what Travis would do
+# Run integrations locally
 
+# Import environment variables
 . ./env.sh
 
+# Build the project and the spring boot bundle
 mvn clean install -Dmaven.test.skip=true -pl \!integration-provisioning
 
-# Download certs... we'll skip this as we already have them in a local ./certs folder
-# curl -s https://raw.githubusercontent.com/symphonyoss/contrib-toolbox/master/scripts/download-files.sh | bash
-
-# Inject vars
+# Inject environment variables in application.yaml
 rm -rf application.yaml
 curl -s https://raw.githubusercontent.com/symphonyoss/contrib-toolbox/master/scripts/inject-vars.sh | bash -s -- ./docs/configuration/application.yaml.template application.yaml
 
+# Cleanup tomcat folder from previous runs
 rm -rf tomcat ; mkdir tomcat
 
-java $LIBS_PATH \
--Dlogs.basedir=target \
--Dlog4j.configurationFile=./docs/configuration/log4j.properties \
--jar integration-web/target/integration.jar \
---spring.config.location=$PWD \
+# Run the Spring Boot application
+java -Dlog4j2.outputAllToConsole=true -Dlogs.basedir=target \
+-agentlib:jdwp=transport=dt_socket,server=y,address=5000,suspend=n \
+-jar target/integration.jar \
 --server.tomcat.basedir=$PWD/tomcat
